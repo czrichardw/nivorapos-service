1dc4fdc84a1b77f4b45944d339058c2ba3db51a7
richard483 <richard.william483@gmail.com>
2026-04-28 16:56:58 +0700

    feat: add method security configuration and toggle for preauthorization

--- src/main/kotlin/id/nivorapos/pos_service/config/SecurityConfig.kt
+++ src/main/kotlin/id/nivorapos/pos_service/config/SecurityConfig.kt
@@ -8,7 +8,6 @@ import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -19,7 +18,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
 class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
     private val userDetailsService: UserDetailsService
